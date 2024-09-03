19fc7fe1923764d6ae2311d9ab3ddcf4964d9374
JorLojor <nugas123man@gmail.com>
2024-09-03 17:53:18 +0700

    seeeep

--- m01-connect_to_db/cmd/main.go
+++ m01-connect_to_db/cmd/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"m01-connect_to_db/config"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
+	config.ConnectToPostgresql()
+	defer config.CloseConnection()
 
-	log.Println("Starting server on :8765")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var greeting string
+		err := config.Connection.QueryRow(context.Background(), "SELECT 'Hello, PostgreSQL!'").Scan(&greeting)
+		if err != nil {
+			http.Error(w, "Failed to query database", http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprint(w, greeting)
+	})
+	log.Println("Starting server on : http://localhost:8765")
 	err := http.ListenAndServe(":8765", nil)
 	if err != nil {
 		log.Fatal(err)
